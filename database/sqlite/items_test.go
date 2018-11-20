511b98ab7c36c061b57ad7469230910fda64edb3
Jeffrey D <verygoodsoftwarenotvirus@protonmail.com>
2018-11-20 22:20:36 +0000

    CI

---
+++ database/sqlite/items_test.go
@@ -0,0 +1,35 @@
+package sqlite_test
+
+import (
+	"testing"
+)
+
+func TestSqlite_GetItem(T *testing.T) {
+	T.Parallel()
+
+	T.Skip("Unimplemented test")
+}
+
+func TestSqlite_GetItems(T *testing.T) {
+	T.Parallel()
+
+	T.Skip("Unimplemented test")
+}
+
+func TestSqlite_CreateItem(T *testing.T) {
+	T.Parallel()
+
+	T.Skip("Unimplemented test")
+}
+
+func TestSqlite_UpdateItem(T *testing.T) {
+	T.Parallel()
+
+	T.Skip("Unimplemented test")
+}
+
+func TestSqlite_DeleteItem(T *testing.T) {
+	T.Parallel()
+
+	T.Skip("Unimplemented test")
+}
---
+++ services/items/http_routes_test.go
@@ -0,0 +1,35 @@
+package items_test
+
+import (
+	"testing"
+)
+
+func TestItemsService_Read(T *testing.T) {
+	T.Parallel()
+
+	T.Skip("Unimplemented test")
+}
+
+func TestItemsService_List(T *testing.T) {
+	T.Parallel()
+
+	T.Skip("Unimplemented test")
+}
+
+func TestItemsService_Delete(T *testing.T) {
+	T.Parallel()
+
+	T.Skip("Unimplemented test")
+}
+
+func TestItemsService_Update(T *testing.T) {
+	T.Parallel()
+
+	T.Skip("Unimplemented test")
+}
+
+func TestItemsService_Create(T *testing.T) {
+	T.Parallel()
+
+	T.Skip("Unimplemented test")
+}

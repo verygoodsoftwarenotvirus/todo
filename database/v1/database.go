ef126846dcc9b856baef0bc749200e61f63db2c9
Jeffrey D <1845584-verygoodsoftwarenotvirus@users.noreply.gitlab.com>
2019-08-16 20:26:42 -0500

    reorg a file

--- database/v1/database.go
+++ database/v1/database.go
@@ -7,7 +7,28 @@ import (
 	"gitlab.com/verygoodsoftwarenotvirus/todo/models/v1"
 )
 
+var (
+	_ Scanner = (*sql.Row)(nil)
+	_ Querier = (*sql.DB)(nil)
+	_ Querier = (*sql.Tx)(nil)
+)
+
 type (
+	// Scanner represents any database response (i.e. sql.Row[s])
+	Scanner interface {
+		Scan(dest ...interface{}) error
+	}
+
+	// Querier is a subset interface for sql.{DB|Tx} objects
+	Querier interface {
+		ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
+		QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
+		QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
+	}
+
+	// ConnectionDetails is a string alias for dependency injection
+	ConnectionDetails string
+
 	// Database describes anything that stores data for our services
 	Database interface {
 		Migrate(ctx context.Context) error
@@ -18,24 +39,4 @@ type (
 		models.OAuth2ClientDataManager
 		models.WebhookDataManager
 	}
-
-	// ConnectionDetails is a string alias for a Postgres url
-	ConnectionDetails string
 )
-
-var _ Scanner = (*sql.Row)(nil)
-
-// Scanner represents any database response (i.e. sql.Row[s])
-type Scanner interface {
-	Scan(dest ...interface{}) error
-}
-
-var _ Querier = (*sql.DB)(nil)
-var _ Querier = (*sql.Tx)(nil)
-
-// Querier is a subset interface for sql.{DB|Tx} objects
-type Querier interface {
-	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
-	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
-	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
-}

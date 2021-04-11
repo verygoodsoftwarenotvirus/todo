366b9316b9f08e1fa9be26e8ae21b20591ef46b1
Jeffrey D <verygoodsoftwarenotvirus@protonmail.com>
2021-04-11 09:55:59 -0500

    add zero tests to permissions

---
+++ internal/app/server/server_test.go
@@ -0,0 +1,13 @@
+package server
+
+import (
+	"testing"
+
+	// httpserver "gitlab.com/verygoodsoftwarenotvirus/todo/internal/app/server/http"
+	// "gitlab.com/verygoodsoftwarenotvirus/todo/internal/pkg/config"
+)
+
+func TestProvideServer(t *testing.T) {
+	// TODO: test me!
+	ProvideServer(nil, nil)
+}

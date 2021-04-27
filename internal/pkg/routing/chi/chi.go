5ae0f5b36a242f35a131641c6b0c4e2495d29edf
Jeffrey D <verygoodsoftwarenotvirus@protonmail.com>
2021-04-26 22:44:20 -0500

    okay last one

--- internal/pkg/routing/chi/chi.go
+++ internal/pkg/routing/chi/chi.go
@@ -19,8 +19,9 @@ import (
 )
 
 const (
-	maxTimeout = 120 * time.Second
-	maxCORSAge = 300
+	roughlyOneYear = time.Hour * 24 * 365
+	maxTimeout     = 120 * time.Second
+	maxCORSAge     = 300
 )
 
 var (
@@ -59,32 +60,31 @@ func buildChiMux(logger logging.Logger) chi.Router {
 	})
 
 	s := secure.New(secure.Options{
-		AllowedHosts:            []string{""},                                    // AllowedHosts is a list of fully qualified domain names that are allowed. Default is empty list, which allows any and all host names.
-		AllowedHostsAreRegex:    false,                                           // AllowedHostsAreRegex determines, if the provided AllowedHosts slice contains valid regular expressions. Default is false.
-		HostsProxyHeaders:       []string{"X-Forwarded-Hosts"},                   // HostsProxyHeaders is a set of header keys that may hold a proxied hostname value for the request.
-		SSLRedirect:             true,                                            // If SSLRedirect is set to true, then only allow HTTPS requests. Default is false.
-		SSLTemporaryRedirect:    false,                                           // If SSLTemporaryRedirect is true, the a 302 will be used while redirecting. Default is false (301).
-		SSLHost:                 "",                                              // SSLHost is the host name that is used to redirect HTTP requests to HTTPS. Default is "", which indicates to use the same host.
-		SSLHostFunc:             nil,                                             // SSLHostFunc is a function pointer, the return value of the function is the host name that has same functionality as `SSHost`. Default is nil. If SSLHostFunc is nil, the `SSLHost` option will be used.
-		SSLProxyHeaders:         map[string]string{"X-Forwarded-Proto": "https"}, // SSLProxyHeaders is set of header keys with associated values that would indicate a valid HTTPS request. Useful when using Nginx: `map[string]string{"X-Forwarded-Proto": "https"}`. Default is blank map.
-		STSSeconds:              int64((time.Hour * 24 * 365).Seconds()),         // STSSeconds is the max-age of the Strict-Transport-Security header. Default is 0, which would NOT include the header.
-		STSIncludeSubdomains:    true,                                            // If STSIncludeSubdomains is set to true, the `includeSubdomains` will be appended to the Strict-Transport-Security header. Default is false.
-		STSPreload:              true,                                            // If STSPreload is set to true, the `preload` flag will be appended to the Strict-Transport-Security header. Default is false.
-		ForceSTSHeader:          false,                                           // STS header is only included when the connection is HTTPS. If you want to force it to always be added, set to true. `IsDevelopment` still overrides this. Default is false.
-		FrameDeny:               true,                                            // If FrameDeny is set to true, adds the X-Frame-Options header with the value of `DENY`. Default is false.
-		CustomFrameOptionsValue: "",                                              // CustomFrameOptionsValue allows the X-Frame-Options header value to be set with a custom value. This overrides the FrameDeny option. Default is "".
-		ContentTypeNosniff:      true,                                            // If ContentTypeNosniff is true, adds the X-Content-Type-Options header with the value `nosniff`. Default is false.
-		BrowserXssFilter:        true,                                            // If BrowserXssFilter is true, adds the X-XSS-Protection header with the value `1; mode=block`. Default is false.
-		CustomBrowserXssValue:   "",                                              // CustomBrowserXssValue allows the X-XSS-Protection header value to be set with a custom value. This overrides the BrowserXssFilter option. Default is "".
-		ContentSecurityPolicy:   "",                                              // ContentSecurityPolicy allows the Content-Security-Policy header value to be set with a custom value. Default is "". Passing a template string will replace `$NONCE` with a dynamic nonce value of 16 bytes for each request which can be later retrieved using the Nonce function.
-		PublicKey:               "",                                              // Deprecated: This feature is no longer recommended. PublicKey implements HPKP to prevent MITM attacks with forged certificates. Default is "".
-		ReferrerPolicy:          "",                                              // ReferrerPolicy allows the Referrer-Policy header with the value to be set with a custom value. Default is "".
-		FeaturePolicy:           "",                                              // Deprecated: this header has been renamed to PermissionsPolicy. FeaturePolicy allows the Feature-Policy header with the value to be set with a custom value. Default is "".
-		ExpectCTHeader:          "",                                              // ExpectCTHeader allows the Expect-CT header value to be set with a custom value. Default is "".
-		SecureContextKey:        "secureContext",                                 // SecureContextKey allows a custom key to be specified for context storage.
-		IsDevelopment:           true,                                            // This will cause the AllowedHosts, SSLRedirect, and STSSeconds/STSIncludeSubdomains options to be ignored during development. When deploying to production, be sure to set this to false.
+		AllowedHosts:            []string{""},
+		AllowedHostsAreRegex:    false,
+		HostsProxyHeaders:       []string{"X-Forwarded-Hosts"},
+		SSLRedirect:             true,
+		SSLTemporaryRedirect:    false,
+		SSLHost:                 "",
+		SSLHostFunc:             nil,
+		SSLProxyHeaders:         map[string]string{"X-Forwarded-Proto": "https"},
+		STSSeconds:              int64(roughlyOneYear.Seconds()),
+		STSIncludeSubdomains:    true,
+		STSPreload:              true,
+		ForceSTSHeader:          false,
+		FrameDeny:               true,
+		CustomFrameOptionsValue: "",
+		ContentTypeNosniff:      true,
+		BrowserXssFilter:        true,
+		CustomBrowserXssValue:   "",
+		ContentSecurityPolicy:   "",
+		PublicKey:               "",
+		ReferrerPolicy:          "",
+		FeaturePolicy:           "",
+		ExpectCTHeader:          "",
+		SecureContextKey:        "secureContext",
+		IsDevelopment:           true,
 	})
-	logger = logging.EnsureLogger(logger)
 
 	mux := chi.NewRouter()
 	mux.Use(
@@ -92,7 +92,7 @@ func buildChiMux(logger logging.Logger) chi.Router {
 		chimiddleware.RequestID,
 		chimiddleware.RealIP,
 		chimiddleware.Timeout(maxTimeout),
-		logging.BuildLoggingMiddleware(logger.WithName("router")),
+		logging.BuildLoggingMiddleware(logging.EnsureLogger(logger).WithName("router")),
 		ch.Handler,
 	)
 

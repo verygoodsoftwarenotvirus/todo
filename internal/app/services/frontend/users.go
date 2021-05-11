d3cefdb7946b4f6e0e33b510ae690939e6e24edb
Jeffrey D <verygoodsoftwarenotvirus@protonmail.com>
2021-05-11 01:56:42 -0500

    consolidate users routes

--- internal/app/services/frontend/users.go
+++ internal/app/services/frontend/users.go
@@ -40,7 +40,7 @@ func (s *Service) fetchUsers(ctx context.Context, req *http.Request) (users *typ
 //go:embed templates/partials/generated/tables/users_table.gotpl
 var usersTableTemplate string
 
-func (s *Service) buildUsersTableView(includeBaseTemplate bool) func(http.ResponseWriter, *http.Request) {
+func (s *Service) buildUsersTableView(includeBaseTemplate, forSearch bool) func(http.ResponseWriter, *http.Request) {
 	return func(res http.ResponseWriter, req *http.Request) {
 		ctx, span := s.tracer.StartSpan(req.Context())
 		defer span.End()
@@ -54,77 +54,24 @@ func (s *Service) buildUsersTableView(includeBaseTemplate bool) func(http.Respon
 			return
 		}
 
-		users, err := s.fetchUsers(ctx, req)
-		if err != nil {
-			observability.AcknowledgeError(err, logger, span, "fetching users from datastore")
-			res.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		tmplFuncMap := map[string]interface{}{
-			"individualURL": func(x *types.User) template.URL {
-				/* #nosec G203 */
-				return template.URL(fmt.Sprintf("/dashboard_pages/admin/users/%d", x.ID))
-			},
-			"pushURL": func(x *types.User) template.URL {
-				/* #nosec G203 */
-				return template.URL(fmt.Sprintf("/admin/users/%d", x.ID))
-			},
-		}
-
-		if includeBaseTemplate {
-			tmpl := s.renderTemplateIntoBaseTemplate(usersTableTemplate, tmplFuncMap)
-
-			page := &pageData{
-				IsLoggedIn:  sessionCtxData != nil,
-				Title:       "Users",
-				ContentData: users,
-			}
-			if sessionCtxData != nil {
-				page.IsServiceAdmin = sessionCtxData.Requester.ServiceAdminPermission.IsServiceAdmin()
-			}
-
-			if err = s.renderTemplateToResponse(tmpl, page, res); err != nil {
-				observability.AcknowledgeError(err, logger, span, "rendering users dashboard tmpl")
+		var users *types.UserList
+		if forSearch {
+			query := req.URL.Query().Get(types.SearchQueryKey)
+			searchResults, err := s.dataStore.SearchForUsersByUsername(ctx, query)
+			if err != nil {
+				observability.AcknowledgeError(err, logger, span, "fetching users from datastore")
 				res.WriteHeader(http.StatusInternalServerError)
 				return
 			}
+
+			users = &types.UserList{Users: searchResults}
 		} else {
-			tmpl := s.parseTemplate("dashboard", usersTableTemplate, tmplFuncMap)
-
-			if err = s.renderTemplateToResponse(tmpl, users, res); err != nil {
-				observability.AcknowledgeError(err, logger, span, "rendering users table view")
+			users, err = s.fetchUsers(ctx, req)
+			if err != nil {
+				observability.AcknowledgeError(err, logger, span, "fetching users from datastore")
 				res.WriteHeader(http.StatusInternalServerError)
 				return
 			}
-		}
-	}
-}
-
-func (s *Service) buildUserSearchResults(includeBaseTemplate bool) func(http.ResponseWriter, *http.Request) {
-	return func(res http.ResponseWriter, req *http.Request) {
-		ctx, span := s.tracer.StartSpan(req.Context())
-		defer span.End()
-
-		logger := s.logger.WithRequest(req)
-
-		sessionCtxData, err := s.sessionContextDataFetcher(req)
-		if err != nil {
-			observability.AcknowledgeError(err, logger, span, "no session context data attached to request")
-			http.Redirect(res, req, "/login", http.StatusSeeOther)
-			return
-		}
-
-		query := req.URL.Query().Get(types.SearchQueryKey)
-		searchResults, err := s.dataStore.SearchForUsersByUsername(ctx, query)
-		if err != nil {
-			observability.AcknowledgeError(err, logger, span, "fetching users from datastore")
-			res.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-
-		users := &types.UserList{
-			Users: searchResults,
 		}
 
 		tmplFuncMap := map[string]interface{}{

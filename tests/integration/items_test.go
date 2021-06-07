360394e11475109d702c38bfd88384c96586cfbc
Jeffrey D <verygoodsoftwarenotvirus@protonmail.com>
2021-06-06 21:53:04 -0500

    integration test comment updates

--- tests/integration/items_test.go
+++ tests/integration/items_test.go
@@ -17,7 +17,7 @@ func checkItemEquality(t *testing.T, expected, actual *types.Item) {
 	t.Helper()
 
 	assert.NotZero(t, actual.ID)
-	assert.Equal(t, expected.Name, actual.Name, "expected BucketName for item #%d to be %v, but it was %v ", expected.ID, expected.Name, actual.Name)
+	assert.Equal(t, expected.Name, actual.Name, "expected Name for item #%d to be %v, but it was %v ", expected.ID, expected.Name, actual.Name)
 	assert.Equal(t, expected.Details, actual.Details, "expected Details for item #%d to be %v, but it was %v ", expected.ID, expected.Details, actual.Details)
 	assert.NotZero(t, actual.CreatedOn)
 }
@@ -35,7 +35,7 @@ func (s *TestSuite) TestItems_Creating() {
 			createdItem, err := testClients.main.CreateItem(ctx, exampleItemInput)
 			requireNotNilAndNoProblems(t, createdItem, err)
 
-			// Assert item equality.
+			// assert item equality
 			checkItemEquality(t, exampleItem, createdItem)
 
 			auditLogEntries, err := testClients.admin.GetAuditLogForItem(ctx, createdItem.ID)
@@ -46,7 +46,7 @@ func (s *TestSuite) TestItems_Creating() {
 			}
 			validateAuditLogEntries(t, expectedAuditLogEntries, auditLogEntries, createdItem.ID, audit.ItemAssignmentKey)
 
-			// Clean up.
+			// clean up
 			assert.NoError(t, testClients.main.ArchiveItem(ctx, createdItem.ID))
 		}
 	})
@@ -60,19 +60,19 @@ func (s *TestSuite) TestItems_Listing() {
 			ctx, span := tracing.StartCustomSpan(s.ctx, t.Name())
 			defer span.End()
 
-			// Create items.
+			// create items
 			var expected []*types.Item
 			for i := 0; i < 5; i++ {
-				// Create item.
 				exampleItem := fakes.BuildFakeItem()
 				exampleItemInput := fakes.BuildFakeItemCreationInputFromItem(exampleItem)
+
 				createdItem, itemCreationErr := testClients.main.CreateItem(ctx, exampleItemInput)
 				requireNotNilAndNoProblems(t, createdItem, itemCreationErr)
 
 				expected = append(expected, createdItem)
 			}
 
-			// Assert item list equality.
+			// assert item list equality
 			actual, err := testClients.main.GetItems(ctx, nil)
 			requireNotNilAndNoProblems(t, actual, err)
 			assert.True(
@@ -83,7 +83,7 @@ func (s *TestSuite) TestItems_Listing() {
 				len(actual.Items),
 			)
 
-			// Clean up.
+			// clean up
 			for _, createdItem := range actual.Items {
 				assert.NoError(t, testClients.main.ArchiveItem(ctx, createdItem.ID))
 			}
@@ -99,13 +99,13 @@ func (s *TestSuite) TestItems_Searching() {
 			ctx, span := tracing.StartCustomSpan(s.ctx, t.Name())
 			defer span.End()
 
-			// Create items.
+			// create items
 			exampleItem := fakes.BuildFakeItem()
 			var expected []*types.Item
 			for i := 0; i < 5; i++ {
-				// Create item.
 				exampleItemInput := fakes.BuildFakeItemCreationInputFromItem(exampleItem)
 				exampleItemInput.Name = fmt.Sprintf("%s %d", exampleItemInput.Name, i)
+
 				createdItem, itemCreationErr := testClients.main.CreateItem(ctx, exampleItemInput)
 				requireNotNilAndNoProblems(t, createdItem, itemCreationErr)
 
@@ -114,7 +114,7 @@ func (s *TestSuite) TestItems_Searching() {
 
 			exampleLimit := uint8(20)
 
-			// Assert item list equality.
+			// assert item list equality
 			actual, err := testClients.main.SearchItems(ctx, exampleItem.Name, exampleLimit)
 			requireNotNilAndNoProblems(t, actual, err)
 			assert.True(
@@ -125,7 +125,7 @@ func (s *TestSuite) TestItems_Searching() {
 				len(actual),
 			)
 
-			// Clean up.
+			// clean up
 			for _, createdItem := range expected {
 				assert.NoError(t, testClients.main.ArchiveItem(ctx, createdItem.ID))
 			}
@@ -141,13 +141,13 @@ func (s *TestSuite) TestItems_Searching_ReturnsOnlyItemsThatBelongToYou() {
 			ctx, span := tracing.StartCustomSpan(s.ctx, t.Name())
 			defer span.End()
 
-			// Create items.
+			// create items
 			exampleItem := fakes.BuildFakeItem()
 			var expected []*types.Item
 			for i := 0; i < 5; i++ {
-				// Create item.
 				exampleItemInput := fakes.BuildFakeItemCreationInputFromItem(exampleItem)
 				exampleItemInput.Name = fmt.Sprintf("%s %d", exampleItemInput.Name, i)
+
 				createdItem, itemCreationErr := testClients.main.CreateItem(ctx, exampleItemInput)
 				requireNotNilAndNoProblems(t, createdItem, itemCreationErr)
 
@@ -156,7 +156,7 @@ func (s *TestSuite) TestItems_Searching_ReturnsOnlyItemsThatBelongToYou() {
 
 			exampleLimit := uint8(20)
 
-			// Assert item list equality.
+			// assert item list equality
 			actual, err := testClients.main.SearchItems(ctx, exampleItem.Name, exampleLimit)
 			requireNotNilAndNoProblems(t, actual, err)
 			assert.True(
@@ -167,7 +167,7 @@ func (s *TestSuite) TestItems_Searching_ReturnsOnlyItemsThatBelongToYou() {
 				len(actual),
 			)
 
-			// Clean up.
+			// clean up
 			for _, createdItem := range expected {
 				assert.NoError(t, testClients.main.ArchiveItem(ctx, createdItem.ID))
 			}
@@ -183,7 +183,6 @@ func (s *TestSuite) TestItems_ExistenceChecking_ReturnsFalseForNonexistentItem()
 			ctx, span := tracing.StartCustomSpan(s.ctx, t.Name())
 			defer span.End()
 
-			// Attempt to fetch nonexistent item.
 			actual, err := testClients.main.ItemExists(ctx, nonexistentID)
 			assert.NoError(t, err)
 			assert.False(t, actual)
@@ -199,18 +198,18 @@ func (s *TestSuite) TestItems_ExistenceChecking_ReturnsTrueForValidItem() {
 			ctx, span := tracing.StartCustomSpan(s.ctx, t.Name())
 			defer span.End()
 
-			// Create item.
+			// create item
 			exampleItem := fakes.BuildFakeItem()
 			exampleItemInput := fakes.BuildFakeItemCreationInputFromItem(exampleItem)
 			createdItem, err := testClients.main.CreateItem(ctx, exampleItemInput)
 			requireNotNilAndNoProblems(t, createdItem, err)
 
-			// Fetch item.
+			// retrieve item
 			actual, err := testClients.main.ItemExists(ctx, createdItem.ID)
 			assert.NoError(t, err)
 			assert.True(t, actual)
 
-			// Clean up item.
+			// clean up item
 			assert.NoError(t, testClients.main.ArchiveItem(ctx, createdItem.ID))
 		}
 	})
@@ -224,7 +223,6 @@ func (s *TestSuite) TestItems_Reading_Returns404ForNonexistentItem() {
 			ctx, span := tracing.StartCustomSpan(s.ctx, t.Name())
 			defer span.End()
 
-			// Attempt to fetch nonexistent item.
 			_, err := testClients.main.GetItem(ctx, nonexistentID)
 			assert.Error(t, err)
 		}
@@ -239,20 +237,20 @@ func (s *TestSuite) TestItems_Reading() {
 			ctx, span := tracing.StartCustomSpan(s.ctx, t.Name())
 			defer span.End()
 
-			// Create item.
+			// create item
 			exampleItem := fakes.BuildFakeItem()
 			exampleItemInput := fakes.BuildFakeItemCreationInputFromItem(exampleItem)
 			createdItem, err := testClients.main.CreateItem(ctx, exampleItemInput)
 			requireNotNilAndNoProblems(t, createdItem, err)
 
-			// Fetch item.
+			// retrieve item
 			actual, err := testClients.main.GetItem(ctx, createdItem.ID)
 			requireNotNilAndNoProblems(t, actual, err)
 
-			// Assert item equality.
+			// assert item equality
 			checkItemEquality(t, exampleItem, actual)
 
-			// Clean up item.
+			// clean up item
 			assert.NoError(t, testClients.main.ArchiveItem(ctx, createdItem.ID))
 		}
 	})
@@ -290,21 +288,21 @@ func (s *TestSuite) TestItems_Updating() {
 			ctx, span := tracing.StartCustomSpan(s.ctx, t.Name())
 			defer span.End()
 
-			// Create item.
+			// create item
 			exampleItem := fakes.BuildFakeItem()
 			exampleItemInput := fakes.BuildFakeItemCreationInputFromItem(exampleItem)
 			createdItem, err := testClients.main.CreateItem(ctx, exampleItemInput)
 			requireNotNilAndNoProblems(t, createdItem, err)
 
-			// Change item.
+			// change item
 			createdItem.Update(convertItemToItemUpdateInput(exampleItem))
 			assert.NoError(t, testClients.main.UpdateItem(ctx, createdItem))
 
-			// Fetch item.
+			// retrieve changed item
 			actual, err := testClients.main.GetItem(ctx, createdItem.ID)
 			requireNotNilAndNoProblems(t, actual, err)
 
-			// Assert item equality.
+			// assert item equality
 			checkItemEquality(t, exampleItem, actual)
 			assert.NotNil(t, actual.LastUpdatedOn)
 
@@ -317,7 +315,7 @@ func (s *TestSuite) TestItems_Updating() {
 			}
 			validateAuditLogEntries(t, expectedAuditLogEntries, auditLogEntries, createdItem.ID, audit.ItemAssignmentKey)
 
-			// Clean up item.
+			// clean up item
 			assert.NoError(t, testClients.main.ArchiveItem(ctx, createdItem.ID))
 		}
 	})
@@ -344,13 +342,13 @@ func (s *TestSuite) TestItems_Archiving() {
 			ctx, span := tracing.StartCustomSpan(s.ctx, t.Name())
 			defer span.End()
 
-			// Create item.
+			// create item
 			exampleItem := fakes.BuildFakeItem()
 			exampleItemInput := fakes.BuildFakeItemCreationInputFromItem(exampleItem)
 			createdItem, err := testClients.main.CreateItem(ctx, exampleItemInput)
 			requireNotNilAndNoProblems(t, createdItem, err)
 
-			// Clean up item.
+			// clean up item
 			assert.NoError(t, testClients.main.ArchiveItem(ctx, createdItem.ID))
 
 			auditLogEntries, err := testClients.admin.GetAuditLogForItem(ctx, createdItem.ID)
